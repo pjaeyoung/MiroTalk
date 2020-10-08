5efcf815d75fff502055064a277511e9d9dbba99
pjaeyoung <saiei2484@gmail.com>
2020-10-08 13:40:41 +0900

    feat : Header 컴포넌트 생성 (기능X)

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+export function EditHeader(): JSX.Element {
+  return (
+    <>
+      <button>설정버튼</button>
+      <img src="" alt="MiroTalk Logo" />
+      <div>
+        <button>녹화</button>
+        <button>채팅</button>
+      </div>
+    </>
+  );
+}
+
+export function ModeHeader(): JSX.Element {
+  return <></>;
+}
