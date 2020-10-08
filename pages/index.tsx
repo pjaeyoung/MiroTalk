d5e8b3273afc8ad2790db1df384890c7f4f1bf3d
pjaeyoung <saiei2484@gmail.com>
2020-10-08 13:58:46 +0900

    feat : input에 입력한 질문 state에 저장

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,23 @@ type Question = {
 };
 
 export default function App(): JSX.Element {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [input, setInput] = useState<string>('');
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuestions((prev) => {
+      let lastId = 0;
+      if (prev.length !== 0) {
+        lastId = prev[prev.length - 1].id + 1;
+      }
+      return [...prev, { id: lastId, text: input, isSelected: false }];
+    });
+    setInput('');
+  };
+
   return (
     <Layout header={<EditHeader />}>
       <>
@@ -22,7 +39,11 @@ export default function App(): JSX.Element {
           onChange={onChange}
           onSubmit={onSubmit}
         />
-        <List />
+        <List>
+          {questions.map((question) => (
+            <QuestionListItem key={question.id} question={question} />
+          ))}
+        </List>
       </>
     </Layout>
   );
