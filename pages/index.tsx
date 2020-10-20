c206c0e708715a9c85e34a0bfdd2d84d0a1fb66f
pjaeyoung <saiei2484@gmail.com>
2020-10-20 21:35:51 +0900

    fix : next에서 제공하는 Head 안 써서 생긴 오류 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
+import Head from 'next/head';
 import QuestionCreate from './QuestionCreate';
 
 export default function Home(): JSX.Element {
   return (
     <>
-      <head>
+      <Head>
         <title>MiroTalk</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@700&display=swap"
           rel="stylesheet"
         />
-      </head>
+      </Head>
       <QuestionCreate />
     </>
   );
