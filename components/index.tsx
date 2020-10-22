50f20965af40b56f9e0f47b8ff169ad20b7e236a
pjaeyoung <saiei2484@gmail.com>
2020-10-22 21:03:21 +0900

    feat : ModalTitle 컴포넌트 생성

--- components/index.tsx
+++ components/index.tsx
@@ -8,3 +8,4 @@ export { default as QuestionGroup } from './QuestionGroup';
 export { default as QuestionCreateMain } from './QuestionCreateMain';
 export { default as Modal } from './Modal';
 export { default as CloseModalButton } from './CloseModalButton';
+export { default as ModalTitle } from './ModalTitle';
